78bed8afc238f730c6d7700cb13866070a66aed5
unknown <anuraggolu123@gmail.com>
2024-03-14 17:55:38 +0530

    some chages made regarding file tree

--- frontend/src/local-module/App.jsx
+++ frontend/src/local-module/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-//  import HomePage from './HomePage.jsx';
- import SignUpLogin from './SignUpLogin.jsx';
+ import HomePage from './home/HomePage.jsx';
+//  import SignUpLogin from './SignUpLogin.jsx';
 
 export default function App() {
   return (
  <>
- {/* <HomePage/> */}
- <SignUpLogin />
+ <HomePage/>
+ {/* <SignUpLogin /> */}
  </>
   )
 }
