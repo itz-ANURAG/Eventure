6b41243d816f6217406b1646f4558a84022db0e8
Abhi-11-sirius <abhishek.20224004@mnnit.ac.in>
2024-03-16 19:52:02 +0530

    few files frontend added

--- frontend/src/local-module/App.jsx
+++ frontend/src/local-module/App.jsx
@@ -9,27 +9,29 @@ import CreateEvent from './profile/CreateEvent.jsx';
 import MyEvent from './profile/MyEvent.jsx';
 import ViewProfile from './profile/ViewProfile.jsx';
 import Refund from './profile/Refund.jsx';
-
+import Sidebar from './profile/sideBar.jsx'
 
 
 
 
 export default function App() {
   return (
- <>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='team' element={<Team/>}/>
-        <Route path='Signin' element={<SignUpLogin/>}/>
-        <Route path='my-profile' element={<Profile/>}>
-           <Route path='view-profile' element={<ViewProfile/>}></Route>
-           <Route path='myEvents' element={<MyEvent/>}></Route>
-           <Route path='createEvent' element={<CreateEvent/>}></Route>
-           <Route path='refund' element={<Refund/>}></Route>
-           <Route path=' ' element={<HomePage/>}></Route>
-        </Route>
-      </Routes>   
- </>
+  <>
+  <ViewProfile/>
+  <Sidebar/>
+{/* //       <Routes>
+//         <Route path='/' element={<HomePage/>}/>
+//         <Route path='team' element={<Team/>}/>
+//         <Route path='Signin' element={<SignUpLogin/>}/>
+//         <Route path='my-profile' element={<Profile/>}>
+//            <Route path='view-profile' element={<ViewProfile/>}></Route>
+//            <Route path='myEvents' element={<MyEvent/>}></Route>
+//            <Route path='createEvent' element={<CreateEvent/>}></Route>
+//            <Route path='refund' element={<Refund/>}></Route>
+//            <Route path=' ' element={<HomePage/>}></Route>
+//         </Route>
+//       </Routes>    */}
+  </>
   )
 }
 
--- frontend/src/local-module/profile/ViewProfile.jsx
+++ frontend/src/local-module/profile/ViewProfile.jsx
@@ -1,11 +1,23 @@
 // view profile jsx in my profile section
 import React from 'react'
-
+import '../../stylesheets/ViewProfile.css'
+import userImage from '../../photos/harry-potter.gif'
 function ViewProfile() {
   return (
-    <div>
-      <h1>this is the view profile page</h1>
+    <>
+    {/* <div className='container '>
+    <div className='user-profile-card '>
+    <div className='gradient bg-gradient-to-br from-red-950 to-black '></div>
+    <div className="user-info-container  border-4 border-black">
+      <div className='imageOfUser'><img className='user-image justify-center' src={userImage} alt="user"/></div>
+      
+      <div className='user-name text-center whiltespace-wrap p-3'>HARRY POTTER</div>
+      <div className='user-description'>I'M An Actor</div>
+      <div className='email'>Email ID:harry@gmail.com</div>
     </div>
+    </div>
+    </div> */}
+    </>
   )
 }
 
